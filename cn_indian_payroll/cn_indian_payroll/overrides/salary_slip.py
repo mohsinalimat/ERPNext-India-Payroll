2bc2bbb32ad283f0a3ddcbf6a5ae5a013ebb5f10
shinil-cloud <shinilshinu97@gmail.com>
2025-07-27 16:16:59 +0530

    fix:reimbursement depends on payment days payout

--- cn_indian_payroll/cn_indian_payroll/overrides/salary_slip.py
+++ cn_indian_payroll/cn_indian_payroll/overrides/salary_slip.py
@@ -156,105 +156,105 @@ class CustomSalarySlip(SalarySlip):
 
 
 
-    def get_working_days_details(self, lwp=None, for_preview=0):
-        payroll_settings = frappe.get_cached_value(
-            "Payroll Settings",
-            None,
-            (
-                "payroll_based_on",
-                "include_holidays_in_total_working_days",
-                "consider_marked_attendance_on_holidays",
-                "daily_wages_fraction_for_half_day",
-                "consider_unmarked_attendance_as",
-            ),
-            as_dict=1,
-        )
+    # def get_working_days_details(self, lwp=None, for_preview=0):
+    #     payroll_settings = frappe.get_cached_value(
+    #         "Payroll Settings",
+    #         None,
+    #         (
+    #             "payroll_based_on",
+    #             "include_holidays_in_total_working_days",
+    #             "consider_marked_attendance_on_holidays",
+    #             "daily_wages_fraction_for_half_day",
+    #             "consider_unmarked_attendance_as",
+    #         ),
+    #         as_dict=1,
+    #     )
 
-        consider_marked_attendance_on_holidays = (
-            payroll_settings.include_holidays_in_total_working_days
-            and payroll_settings.consider_marked_attendance_on_holidays
-        )
+    #     consider_marked_attendance_on_holidays = (
+    #         payroll_settings.include_holidays_in_total_working_days
+    #         and payroll_settings.consider_marked_attendance_on_holidays
+    #     )
 
-        daily_wages_fraction_for_half_day = flt(payroll_settings.daily_wages_fraction_for_half_day) or 0.5
+    #     daily_wages_fraction_for_half_day = flt(payroll_settings.daily_wages_fraction_for_half_day) or 0.5
 
-        working_days = date_diff(self.end_date, self.start_date) + 1
-        working_days_test=0
-        if for_preview:
-            self.total_working_days = working_days
-            self.payment_days = working_days
-            return
+    #     working_days = date_diff(self.end_date, self.start_date) + 1
+    #     working_days_test=0
+    #     if for_preview:
+    #         self.total_working_days = working_days
+    #         self.payment_days = working_days
+    #         return
 
-        holidays = self.get_holidays_for_employee(self.start_date, self.end_date)
-        working_days_list = [add_days(getdate(self.start_date), days=day) for day in range(0, working_days)]
+    #     holidays = self.get_holidays_for_employee(self.start_date, self.end_date)
+    #     working_days_list = [add_days(getdate(self.start_date), days=day) for day in range(0, working_days)]
 
-        sundays = [d for d in holidays if d.weekday() == 6]
+    #     sundays = [d for d in holidays if d.weekday() == 6]
 
 
-        working_days_test = (working_days-len(sundays))
+    #     working_days_test = (working_days-len(sundays))
 
-        self.custom_working_days_exclude_holiday=working_days_test
+    #     self.custom_working_days_exclude_holiday=working_days_test
 
-        if not cint(payroll_settings.include_holidays_in_total_working_days):
-            working_days_list = [i for i in working_days_list if i not in holidays]
+    #     if not cint(payroll_settings.include_holidays_in_total_working_days):
+    #         working_days_list = [i for i in working_days_list if i not in holidays]
 
-            working_days -= len(holidays)
+    #         working_days -= len(holidays)
 
-            if working_days < 0:
-                frappe.throw(_("There are more holidays than working days this month."))
+    #         if working_days < 0:
+    #             frappe.throw(_("There are more holidays than working days this month."))
 
-        if not payroll_settings.payroll_based_on:
-            frappe.throw(_("Please set Payroll based on in Payroll settings"))
+    #     if not payroll_settings.payroll_based_on:
+    #         frappe.throw(_("Please set Payroll based on in Payroll settings"))
 
-        if payroll_settings.payroll_based_on == "Attendance":
-            actual_lwp, absent = self.calculate_lwp_ppl_and_absent_days_based_on_attendance(
-                holidays, daily_wages_fraction_for_half_day, consider_marked_attendance_on_holidays
-            )
-            self.absent_days = absent
-        else:
-            actual_lwp = self.calculate_lwp_or_ppl_based_on_leave_application(
-                holidays, working_days_list, daily_wages_fraction_for_half_day
-            )
+    #     if payroll_settings.payroll_based_on == "Attendance":
+    #         actual_lwp, absent = self.calculate_lwp_ppl_and_absent_days_based_on_attendance(
+    #             holidays, daily_wages_fraction_for_half_day, consider_marked_attendance_on_holidays
+    #         )
+    #         self.absent_days = absent
+    #     else:
+    #         actual_lwp = self.calculate_lwp_or_ppl_based_on_leave_application(
+    #             holidays, working_days_list, daily_wages_fraction_for_half_day
+    #         )
 
-        if not lwp:
-            lwp = actual_lwp
-        elif lwp != actual_lwp:
-            frappe.msgprint(
-                _("Leave Without Pay does not match with approved {} records").format(
-                    payroll_settings.payroll_based_on
-                )
-            )
+    #     if not lwp:
+    #         lwp = actual_lwp
+    #     elif lwp != actual_lwp:
+    #         frappe.msgprint(
+    #             _("Leave Without Pay does not match with approved {} records").format(
+    #                 payroll_settings.payroll_based_on
+    #             )
+    #         )
 
-        self.leave_without_pay = lwp
-        self.total_working_days = working_days
+    #     self.leave_without_pay = lwp
+    #     self.total_working_days = working_days
 
-        payment_days = self.get_payment_days(payroll_settings.include_holidays_in_total_working_days)
+    #     payment_days = self.get_payment_days(payroll_settings.include_holidays_in_total_working_days)
 
-        if flt(payment_days) > flt(lwp):
-            self.payment_days = flt(payment_days) - flt(lwp)
+    #     if flt(payment_days) > flt(lwp):
+    #         self.payment_days = flt(payment_days) - flt(lwp)
 
-            if payroll_settings.payroll_based_on == "Attendance":
-                self.payment_days -= flt(absent)
+    #         if payroll_settings.payroll_based_on == "Attendance":
+    #             self.payment_days -= flt(absent)
 
-            consider_unmarked_attendance_as = payroll_settings.consider_unmarked_attendance_as or "Present"
+    #         consider_unmarked_attendance_as = payroll_settings.consider_unmarked_attendance_as or "Present"
 
-            if (
-                payroll_settings.payroll_based_on == "Attendance"
-                and consider_unmarked_attendance_as == "Absent"
-            ):
-                unmarked_days = self.get_unmarked_days(
-                    payroll_settings.include_holidays_in_total_working_days, holidays
-                )
-                half_absent_days = self.get_half_absent_days(
-                    payroll_settings.include_holidays_in_total_working_days,
-                    consider_marked_attendance_on_holidays,
-                    holidays,
-                )
-                self.absent_days += (
-                    unmarked_days + half_absent_days * daily_wages_fraction_for_half_day
-                )  # will be treated as absent
-                self.payment_days -= unmarked_days + half_absent_days * daily_wages_fraction_for_half_day
-        else:
-            self.payment_days = 0
+    #         if (
+    #             payroll_settings.payroll_based_on == "Attendance"
+    #             and consider_unmarked_attendance_as == "Absent"
+    #         ):
+    #             unmarked_days = self.get_unmarked_days(
+    #                 payroll_settings.include_holidays_in_total_working_days, holidays
+    #             )
+    #             half_absent_days = self.get_half_absent_days(
+    #                 payroll_settings.include_holidays_in_total_working_days,
+    #                 consider_marked_attendance_on_holidays,
+    #                 holidays,
+    #             )
+    #             self.absent_days += (
+    #                 unmarked_days + half_absent_days * daily_wages_fraction_for_half_day
+    #             )  # will be treated as absent
+    #             self.payment_days -= unmarked_days + half_absent_days * daily_wages_fraction_for_half_day
+    #     else:
+    #         self.payment_days = 0
 
 
 
@@ -344,38 +344,45 @@ class CustomSalarySlip(SalarySlip):
 
         if not self.salary_withholding:
             for earning in self.earnings:
-                component = frappe.get_doc("Salary Component", earning.salary_component)
+                if earning.additional_salary:
+                    get_additional_salary=frappe.get_doc("Additional Salary",earning.additional_salary)
 
-                for reimbursement in ssa_doc.custom_employee_reimbursements or []:
-                    if reimbursement.reimbursements == earning.salary_component:
-                        if self.total_working_days and self.payment_days:
-                            prorated_amount = round(
-                                (reimbursement.monthly_total_amount or 0)
-                                / self.total_working_days
-                                * (self.total_working_days - self.payment_days),
-                                2,
-                            )
+                    if get_additional_salary.ref_doctype=="Employee Benefit Claim":
 
-                            earning.amount -= prorated_amount
+                        component = frappe.get_doc("Salary Component", get_additional_salary.salary_component)
+                        if component.depends_on_payment_days == 1:
+                            for reimbursement in ssa_doc.custom_employee_reimbursements or []:
+                                if reimbursement.reimbursements == get_additional_salary.salary_component:
 
-                for reimbursement in ssa_doc.custom_employee_reimbursements or []:
-                    lta_component = frappe.get_doc(
-                        "Salary Component", reimbursement.reimbursements
-                    )
-                    if lta_component.component_type == "LTA Reimbursement":
-                        if component.component_type in [
-                            "LTA Taxable",
-                            "LTA Non Taxable",
-                        ]:
-                            if self.total_working_days and self.payment_days:
-                                prorated_amount = round(
-                                    (reimbursement.monthly_total_amount or 0)
-                                    / self.total_working_days
-                                    * (self.total_working_days - self.payment_days),
-                                    2,
-                                )
-                                earning.amount -= prorated_amount
-                        break
+                                    if self.total_working_days and self.payment_days:
+
+                                        prorated_amount = round(
+                                            (reimbursement.monthly_total_amount or 0)
+                                            / self.total_working_days
+                                            * (self.total_working_days - self.payment_days),
+                                            2,
+                                        )
+
+                                        earning.amount  = get_additional_salary.amount - prorated_amount
+
+                    if get_additional_salary.ref_doctype == "LTA Claim":
+                        component = frappe.get_doc("Salary Component", get_additional_salary.salary_component)
+
+                        for reimbursement in ssa_doc.custom_employee_reimbursements or []:
+                            lta_component = frappe.get_doc("Salary Component", reimbursement.reimbursements)
+
+                            if lta_component.component_type == "LTA Reimbursement" and \
+                            component.component_type in ["LTA Taxable", "LTA Non Taxable"]:
+
+                                if self.total_working_days and self.payment_days:
+                                    lop_days = self.total_working_days - self.payment_days
+                                    prorated_amount = round(
+                                        (reimbursement.monthly_total_amount or 0) / self.total_working_days * lop_days,
+                                        2
+                                    )
+                                    earning.amount = get_additional_salary.amount - prorated_amount
+                                break
+
 
 
 
